c2ddc4a67e58633d6980d88afd5616b7348214fa
nextgenpathology <nextgenpathology@gmail.com>
2025-07-01 12:50:05 +0530

    final build landing page

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Replace 'your-repo-name' with the name of your GitHub repo
 export default defineConfig({
+  base: '/nextgenpathology.github.io/',
   plugins: [react()],
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+})
