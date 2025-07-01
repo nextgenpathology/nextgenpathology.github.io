a33e76cc740e5bfa700e3e0be73b6be5562c4ad9
nextgenpathology <nextgenpathology@gmail.com>
2025-07-01 13:15:19 +0530

    update

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // Replace 'your-repo-name' with the name of your GitHub repo
 export default defineConfig({
-  base: '/nextgenpathology.github.io/',
+  base: '/',
   plugins: [react()],
 })
